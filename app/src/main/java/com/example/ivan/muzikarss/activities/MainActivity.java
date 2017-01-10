801f5c12008cc75ee18c919790c2e61a875170b8
Ivan <ivanrostohar7@gmail.com>
2017-01-10 11:23:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ivan/muzikarss/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ivan.muzikarss.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ivan.muzikarss.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
